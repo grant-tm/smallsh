99eba2069929935b66b2cf281e04b2076af548ca
grant.tm <grant.t.morgan@gmail.com>
2023-02-17 12:18:20 -0800

    Added variable expansion for instances of 131902 in command and optional items.

--- smallsh.c
+++ smallsh.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
+
 //command object
 struct command{
     char *name; //string representing command name
@@ -78,6 +80,7 @@ void printCommand(struct command* command){
     printf("Background Process: %d\n", command->backgroundProcess);
 }
 
+//Determines if a string is a command or not. Returns 1 if yes, 0 if no.
 int isCommand(char *input){
     if(input == NULL || strlen(input) < strlen("tt")){
         return 0;
@@ -91,6 +94,78 @@ int isCommand(char *input){
     return 1;
 }
 
+
+//Gets string input from user, stores in container.
+char *getInput(char* container){
+    //get user input with fgets, verify input
+    while(!fgets(container, 2048, stdin)){
+        printf("Error: Invalid Input\n");
+    }
+    return container;
+}
+
+//Expands all instances of "$$" in a string into the calling process PID.
+char *expandVariable(char *string){
+    
+    //allocate and intitialize container for variable to expand ("$$")
+    char *varToExpand = malloc(3*sizeof(char));
+    memset(varToExpand, '\0', 3*sizeof(char));
+    strcpy(varToExpand, "$$");
+    
+    //store memory location of first instance of "$$"
+    //if "$$" does not appear in the string result is NULL
+    char *varMemLoc = strstr(string, varToExpand);
+    if(varMemLoc == NULL){
+        return varMemLoc;
+    }
+    
+    //calculate index of beginning of first instance of "$$"
+    size_t varIndex = (size_t)(varMemLoc - string);
+    
+    //string prefix stores all characters preceding first instance of "$$"
+    char *prefix = malloc((varIndex+1) * sizeof(char));
+    memset(prefix, '\0', (varIndex+1) * sizeof(char));
+    strncpy(prefix, string, (varIndex));
+    
+    //string suffix stores all characters after first isntance of "$$"
+    char *suffix = malloc(((strlen(string) - varIndex)-1) * sizeof(char));
+    memset(suffix, '\0', ((strlen(string) - varIndex)-1) * sizeof(char));
+    memcpy(suffix, varMemLoc+2, ((strlen(string) - varIndex)-1));
+    
+    //get PID of calling process as a string
+    //from https://stackoverflow.com/questions/53230155
+    int pid = getpid();
+    char *expandedVar = malloc(25*sizeof(char));
+    memset(expandedVar, '\0', 25*sizeof(char));
+    sprintf(expandedVar, "%d", pid);
+    
+    //concatenate prefix, pid, and suffix in that order
+    prefix = realloc(prefix, strlen(prefix) + strlen(expandedVar) + strlen(suffix) + 2);
+    strcat(prefix, expandedVar);
+    strcat(prefix, suffix);
+    
+    //resize original string and store expanded string
+    string = realloc(string, (strlen(prefix)+1) * sizeof(char));
+    memset(string, '\0', (strlen(prefix)+1) * sizeof(char));
+    strcpy(string, prefix);
+    
+    //free memory
+    free(prefix);
+    free(suffix);
+    free(expandedVar);
+    
+    //recursive step
+    //if expanded string contains more instances of "$$", run again.
+    if(strstr(string, varToExpand) != NULL){
+        free(varToExpand);
+        string = expandVariable(string);
+        return string;
+    }
+    
+    //string contains no more instances of '$$"
+    free(varToExpand);
+    return string;
+}
 
 //Parses string input using tokens to fill in command struct
 struct command *parseCommand(char *commandLineInput){
@@ -111,6 +186,7 @@ struct command *parseCommand(char *commandLineInput){
     token = strtok_r(NULL, " ", &saveptr);
     newCommand->name = calloc(strlen(token) + 1, sizeof(char));
     strcpy(newCommand->name, token);
+    newCommand->name = expandVariable(newCommand->name);
     token = strtok_r(NULL, " ", &saveptr);
     if(token == NULL){
         return newCommand;
@@ -119,11 +195,14 @@ struct command *parseCommand(char *commandLineInput){
     //Token may be argument, "<", ">", or "&" at this point. Token cannot be NULL.
     //Process tokens as arguments until reaching input/output/background flags or end of command.
     int i=0;
-    while(strcmp(token, "<") != 0 && strcmp(token, ">") != 0 && strcmp(token, "&") != 0){
+    printf("token: %s\n", token);
+    printf("background: %d\n", strcmp(token, "&\n") != 0);
+    while(strcmp(token, "<") != 0 && strcmp(token, ">") != 0 && strcmp(token, "&\n") != 0){
  
         //Token is an argument at this point. Store argument and receive next token.
         newCommand->args[i] = calloc(strlen(token) + 1, sizeof(char));
         strcpy(newCommand->args[i], token);
+        newCommand->args[i] = expandVariable(newCommand->args[i]);
         ++i;
         
         //Receive next token. If token is not NULL, process next token.
@@ -131,7 +210,6 @@ struct command *parseCommand(char *commandLineInput){
         if(token == NULL){
             return newCommand;
         }
-        
     }
 
     //Token may be input/output/background flag at this point. Token cannot be NULL.
@@ -140,8 +218,10 @@ struct command *parseCommand(char *commandLineInput){
         
         //Get and store input file name.
         token = strtok_r(NULL, " ", &saveptr);
+        //token = expandVariable(token);
         newCommand->inputFile = calloc(strlen(token)+1, sizeof(char));
         strcpy(newCommand->inputFile, token);
+        newCommand->inputFile = expandVariable(newCommand->inputFile);
         
         //Receive next token. If NULL, end of command is reached.
         token = strtok_r(NULL, " ", &saveptr);
@@ -149,24 +229,25 @@ struct command *parseCommand(char *commandLineInput){
             return newCommand;
         }
     }
-        
     
     //Token may be output/background flag at this point. Token cannot be NULL.
     if(strcmp(token, ">") == 0){
         //Next token is output file name. Process token and receive next token.
         token = strtok_r(NULL, " ", &saveptr);
+        //token = expandVariable(token);
         newCommand->outputFile = calloc(strlen(token)+1, sizeof(char));
         strcpy(newCommand->outputFile, token);
+        newCommand->outputFile = expandVariable(newCommand->outputFile);
         
         //Receive next token. If NULL, end of command is reached.
-        token = strtok_r(NULL, " ", &saveptr);
+        token = strtok_r(NULL, " \n", &saveptr);
         if(token == NULL){
             return newCommand;
         }
     }
 
     //Token may be background flag at this point. Token cannot be NULL.
-    if(strcmp(token, "&") == 0){
+    if(strcmp(token, "&") == 0 || strcmp(token, "&\n") == 0){
         //Mark command to run in the background.
         newCommand->backgroundProcess = 1;
     }
@@ -176,14 +257,20 @@ struct command *parseCommand(char *commandLineInput){
 
 int main(int argc, char *argv[]){
   
-  char input1[2048] = ": commandName arg1 arg2 arg3 < input > output &";
+  struct command* command;
+  char input[2048];
+  int ext_code = 1;
   
-  struct command* newCommand;
-  
-  if(isCommand(input1) == 1){
-      newCommand = parseCommand(input1);
-      printCommand(newCommand);
-      freeCommand(newCommand);
+  while(ext_code){
+      getInput(input);
+      if(isCommand(input) == 1){
+          command = parseCommand(input);
+          printCommand(command);
+          freeCommand(command);
+      }
+      else{
+          ext_code = 0;
+      }
   }
   
   return EXIT_SUCCESS;
