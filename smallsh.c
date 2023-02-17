63143628266c213e6e8c3dc527abf6176b838108
grant.tm <grant.t.morgan@gmail.com>
2023-02-16 16:11:30 -0800

    initial commit

---
+++ smallsh.c
@@ -0,0 +1,13 @@
+#define _GNU_SOURCE
+
+//standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+
+  //edit
+
+  return EXIT_SUCCESS;
+}
